45510aa8071dee32e20cf18a444610dcec8aab3d
mykey007 <mmallowe@gmail.com>
2019-05-11 18:51:47 -0400

    style updates and plumbing for new moviedb feature

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import MovieCard from './components/movieCard'
+import MovieDialog from './components/movieDialouge'
+const apiKey = '32a302191f734d8eebcefdd905b4fa44';
+const OriginalMovies = [
+  {id: 1, title: 'Requiem for a Dream'},
+  {id: 2, title: 'Big Man Japan'},
+  {id: 3, title: 'Twilight Zone'},
+  {id: 4, title: 'Beetlejuice'}
+
+];
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 class App extends Component {
+  //add state to the component
+  state = {movies: [], selectedMovie: null};
+
+  selectMovie = movie => this.setState({selectedMovie: movie});
+  clearMovie = () => this.setState({selectedMovie: null});
+
+  // add lifecycle function to make it async
+  async componentDidMount(){
+    //fake it til you make it
+    // setTimeout (() => 
+    //   // update the state
+    //   this.setState({movies: OriginalMovies}), 2000 
+    // )
+
+
+      //getting real data
+     const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`)
+     const json = await response.json();
+     this.setState({movies:json.results})
+
+
+     // made ajax request via fetch() function
+     // used await in an async function to wait for response
+     // used setState() function once the data is ready
+
+  }
   render() {
+    //read state update here 
+    const {movies, selectedMovie} = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <AppBar position="fixed" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Top Rated Movies
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <div className="App">
+        {movies.map(movie => <MovieCard key={movie.id} movie={movie} selectMovie={this.movie} />)}
+        </div>
+        <MovieDialog movie={selectedMovie} handleClose={this.clearMovie} />
       </div>
     );
   }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
